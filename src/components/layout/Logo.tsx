9da409da535f9e2c2a3ec80f9eb0365c0528e227
Valentin Hăloiu <valentin.haloiu@gmail.com>
2021-04-04 14:12:31 +0200

    Add link to homepage on navbar logo

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'wouter'
+
 export const Logo: React.FC = () => (
-    <div className="flex items-center">
-        <img className="h-8 w-auto" src="/protocols/logo.svg" alt="Logo" />
-        <span
-            className="font-black ml-2 truncate cursor-default"
-            title="Wayland Explorer"
-        >
-            Wayland Explorer
-        </span>
-    </div>
+    <Link href="/">
+        <a className="flex items-center">
+            <img className="h-8 w-auto" src="/protocols/logo.svg" alt="Logo" />
+            <span className="font-black ml-2 truncate" title="Wayland Explorer">
+                Wayland Explorer
+            </span>
+        </a>
+    </Link>
 )
