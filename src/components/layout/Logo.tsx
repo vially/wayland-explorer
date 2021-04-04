38142ec2d7e561cd751cfe6a179657db6c7177db
Valentin Hăloiu <valentin.haloiu@gmail.com>
2021-04-04 18:17:17 +0200

    Downplay header logo boldness

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -4,7 +4,10 @@ export const Logo: React.FC = () => (
     <Link href="/">
         <a href="/" className="flex items-center">
             <img className="h-8 w-auto" src="/protocols/logo.svg" alt="Logo" />
-            <span className="font-black ml-2 truncate" title="Wayland Explorer">
+            <span
+                className="font-semibold ml-2 truncate"
+                title="Wayland Explorer"
+            >
                 Wayland Explorer
             </span>
         </a>
