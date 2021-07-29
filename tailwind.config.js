3b74375359f3a24a60427bb27ee62cfedfbcd61a
Valentin Hăloiu <valentin.haloiu@gmail.com>
2021-07-29 21:21:25 +0100

    Revert dark mode to 'media'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: 'class', // or 'media' or 'class'
+    darkMode: 'media', // or 'media' or 'class'
     theme: {
         extend: {},
         colors: { ...colors, transparent: 'transparent' },
