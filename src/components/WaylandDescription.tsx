52c9b5d33243e249a82bb7dd4438881d26a075d3
bbb651 <bar.ye651@gmail.com>
2024-08-27 17:17:39 +0300

    Render `*` lists in descriptions as `<ul>`
    Fixes #70

--- src/components/WaylandDescription.tsx
+++ src/components/WaylandDescription.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { WaylandDescriptionModel, WaylandElementProps } from './common'
 
 export const WaylandDescription: React.FC<
@@ -10,13 +11,33 @@ export const WaylandDescription: React.FC<
             </div>
         )}
 
-        {element.text?.split('\n\n').map((text, index) => (
-            <p
-                key={index}
-                className="my-3 text-base text-gray-700 dark:text-white"
-            >
-                {text}
-            </p>
-        ))}
+        {element.text?.split('\n\n').map((text, index) => {
+            const { block, list } = parseList(text)
+            return (
+                <p
+                    key={index}
+                    className="my-3 text-base text-gray-700 dark:text-white"
+                >
+                    {block}
+                    {list && (
+                        <ul className="list-disc list-inside">
+                            {list.map((text) => (
+                                <li>{text}</li>
+                            ))}
+                        </ul>
+                    )}
+                </p>
+            )
+        })}
     </div>
 )
+
+function parseList(block: string): { block: string; list?: string[] } {
+    const listStart = block.startsWith("* ") ? 0 : block.indexOf('\n*')
+    if (listStart === -1) {
+        return { block };
+    }
+    const list = block.slice(listStart + 2).split('\n* ');
+    console.log({ block: block.slice(0, listStart) });
+    return { block: block.slice(0, listStart), list }
+}
