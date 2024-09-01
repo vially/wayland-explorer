92da2c493f8f6af5cdb5d7907682d31e70fe086f
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-09-01 03:00:11 +0200

    Rework breadcrumb to allow reuse in gitlab MR viewer

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {
     kdeProtocolsWithHardcodedPrefix,
     urlForWaylandProtocol,
@@ -9,100 +10,101 @@ import {
     WaylandProtocolSource,
 } from '../../model/wayland-protocol-metadata'
 
-export const Breadcrumbs: React.FC<{ metadata: WaylandProtocolMetadata }> = ({
-    metadata,
-}) => {
+const SolidChevronRight: React.FC<{ className: string }> = ({ className }) => {
+    // Heroicon name: solid/chevron-right
+    return (
+        <svg
+            className={`shrink-0 h-5 w-5 text-gray-400 ${className}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+        >
+            <path
+                fillRule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+            />
+        </svg>
+    )
+}
+
+export const BreadcrumbSection: React.FC<{
+    children?: React.ReactNode
+    href?: string
+}> = ({ children, href }) =>
+    href ? (
+        <a
+            href={href}
+            target="_blank"
+            rel="noreferrer"
+            className="text-sm font-medium text-gray-500 hover:text-gray-700 truncate"
+        >
+            {children}
+        </a>
+    ) : (
+        <span className="text-sm text-gray-500">{children}</span>
+    )
+
+export const WaylandBreadcrumbs: React.FC<{
+    metadata: WaylandProtocolMetadata
+}> = ({ metadata }) => {
     const xmlFileBaseName =
         metadata.source === WaylandProtocolSource.KDEProtocols &&
         !kdeProtocolsWithHardcodedPrefix.includes(metadata.id)
             ? metadata.id.substring(4)
             : metadata.id
 
-    return (
-        <div>
-            <nav aria-label="Breadcrumb">
-                <ol className="flex flex-wrap items-center space-x-1 [&>li]:overflow-x-hidden">
-                    <li>
-                        <div>
-                            {metadata.source !==
-                            WaylandProtocolSource.External ? (
-                                <a
-                                    href={urlForWaylandProtocolSource(
-                                        metadata.source
-                                    )}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className="text-sm/loose text-gray-500 hover:text-gray-700"
-                                >
-                                    {metadata.source}
-                                </a>
-                            ) : (
-                                <span className="text-sm text-gray-500">
-                                    external
-                                </span>
-                            )}
-                        </div>
-                    </li>
-                    {metadata.source !== WaylandProtocolSource.WaylandCore &&
-                        metadata.source !== WaylandProtocolSource.External && (
-                            <li>
-                                <div className="flex items-center">
-                                    {/* Heroicon name: solid/chevron-right */}
-                                    <svg
-                                        className="shrink-0 h-5 w-5 text-gray-400"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                        aria-hidden="true"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                    <a
-                                        href={urlForWaylandProtocolStability(
-                                            metadata
-                                        )}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        className="ml-1 text-sm/loose font-medium text-gray-500 hover:text-gray-700"
-                                    >
-                                        {metadata.stability}
-                                    </a>
-                                </div>
-                            </li>
-                        )}
-                    <li>
-                        <div className="flex items-center">
-                            {/* Heroicon name: solid/chevron-right */}
-                            <svg
-                                className="shrink-0 h-5 w-5 text-gray-400"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
+    const source =
+        metadata.source === WaylandProtocolSource.External ? (
+            <BreadcrumbSection>external</BreadcrumbSection>
+        ) : (
+            <BreadcrumbSection
+                href={urlForWaylandProtocolSource(metadata.source)}
+            >
+                {metadata.source}
+            </BreadcrumbSection>
+        )
 
-                            <a
-                                href={urlForWaylandProtocol(metadata)}
-                                target="_blank"
-                                rel="noreferrer"
-                                className="ml-1 text-sm font-medium text-gray-500 hover:text-gray-700 truncate"
-                            >
-                                {xmlFileBaseName}.xml
-                            </a>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
-        </div>
+    const stability = metadata.source !== WaylandProtocolSource.WaylandCore &&
+        metadata.source !== WaylandProtocolSource.External && (
+            <BreadcrumbSection href={urlForWaylandProtocolStability(metadata)}>
+                {metadata.stability}
+            </BreadcrumbSection>
+        )
+
+    return (
+        <Breadcrumbs>
+            {source}
+            {stability}
+            <BreadcrumbSection href={urlForWaylandProtocol(metadata)}>
+                {xmlFileBaseName}.xml
+            </BreadcrumbSection>
+        </Breadcrumbs>
+    )
+}
+
+export const Breadcrumbs: React.FC<{ children?: React.ReactNode }> = ({
+    children,
+}) => {
+    return (
+        <nav aria-label="Breadcrumb">
+            <ol className="flex flex-wrap items-center space-x-1 [&>li]:overflow-x-hidden">
+                {React.Children.toArray(children).map((child, i) => {
+                    return (
+                        <li>
+                            {i !== 0 ? (
+                                <div className="flex items-center">
+                                    <SolidChevronRight className="mr-1" />
+                                    {child}
+                                </div>
+                            ) : (
+                                child
+                            )}
+                        </li>
+                    )
+                })}
+            </ol>
+        </nav>
     )
 }
