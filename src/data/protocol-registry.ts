e8ce38a2ec6d6fa8129ff7a34b1c574ab6122c2f
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2026-09-01 00:18:13 +0300

    chore: remove chrome-color-management protocol follow chromium

--- src/data/protocol-registry.ts
+++ src/data/protocol-registry.ts
@@ -1089,15 +1089,6 @@ const protocols: WaylandProtocolRegistryItem[] = [
             'https://source.chromium.org/chromium/chromium/src/+/master:components/exo/wayland/protocol/aura-shell.xml',
     },
     {
-        id: 'chrome-color-management',
-        name: 'Chromium color management',
-        source: WaylandProtocolSource.External,
-        stability: WaylandProtocolStability.Unstable,
-        protocol: require('./protocols/chrome-color-management.json'),
-        externalUrl:
-            'https://source.chromium.org/chromium/chromium/src/+/master:components/exo/wayland/protocol/chrome-color-management.xml',
-    },
-    {
         id: 'overlay-prioritizer',
         name: 'Chromium overlay prioritizer',
         source: WaylandProtocolSource.External,
@@ -1167,7 +1158,7 @@ const protocols: WaylandProtocolRegistryItem[] = [
         stability: WaylandProtocolStability.Unstable,
         protocol: require('./protocols/mir-shell-unstable-v1.json'),
         externalUrl:
-            'https://github.com/canonical/mir/blob/v2.17.0/wayland-protocols/mir-shell-unstable-v1.xml',
+            'https://github.com/canonical/mir/blob/main/wayland-protocols/mir-shell-unstable-v1.xml',
     },
     {
         id: 'xapp-shell',
@@ -1176,7 +1167,7 @@ const protocols: WaylandProtocolRegistryItem[] = [
         stability: WaylandProtocolStability.Unstable,
         protocol: require('./protocols/xapp-shell.json'),
         externalUrl:
-            'https://github.com/linuxmint/xapp/blob/3.3.3-unstable/libxapp/xapp-shell.xml',
+            'https://raw.githubusercontent.com/linuxmint/xapp/refs/heads/master/libxapp/xapp-shell.xml',
     },
 ]
 
